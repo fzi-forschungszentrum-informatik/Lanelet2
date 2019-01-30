4fe899060b6c97126a55c9a49258c9293402ac91
Lingguang Wang <lingguang.wang@kit.edu>
2019-01-30 15:22:55 +0100

    Apply suggestion to lanelet2_validation/src/validators/CurvatureTooBig.cpp

--- lanelet2_validation/src/validators/CurvatureTooBig.cpp
+++ lanelet2_validation/src/validators/CurvatureTooBig.cpp
@@ -14,7 +14,7 @@ namespace {
 Issues CurvatureTooBigChecker::operator()(const lanelet::LaneletMap& map) {
     Issues issues;
     for (auto lanelet_it = map.laneletLayer.begin();lanelet_it!=map.laneletLayer.end();lanelet_it++) {
-        auto left_bound_2d = utils::to2D((*lanelet_it).leftBound());
+        auto left_bound_2d = utils::toHybrid(utils::to2D((*lanelet_it).leftBound()));
         auto right_bound_2d = utils::to2D((*lanelet_it).rightBound());
         if(left_bound_2d.size() >= 3){
             double x0, y0, x1, y1, x2, y2, dx1, dy1, ddx1, ddy1, denom, curvature;
@@ -31,9 +31,7 @@ Issues CurvatureTooBigChecker::operator()(const lanelet::LaneletMap& map) {
                 ddy1 = y2 - 2.0 * y1 + y0;
                 // compute "curvature"
                 denom = pow(dx1 * dx1 + dy1 * dy1, 3.0 / 2.0);
-                if (std::fabs(denom) < 1e-20) {
-	             denom = 1e-20;
-                }
+                if (fabs(denom) < 1e-20)
                     denom = 1e-20;
                 curvature = (ddy1 * dx1 - dy1 * ddx1) / denom;
                 if(fabs(curvature) > 0.5){
