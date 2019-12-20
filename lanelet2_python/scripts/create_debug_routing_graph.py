39bc170b990e1f917a5c38d3dc08ee316a8ee19d
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2019-12-20 15:02:44 +0100

    Format python files

--- lanelet2_python/scripts/create_debug_routing_graph.py
+++ lanelet2_python/scripts/create_debug_routing_graph.py
@@ -8,15 +8,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("filename", help="Path to the input osm file")
     parser.add_argument("output", help="Path to resulting debug routing graph")
-    parser.add_argument("--participant", help="traffic participant type (one of vehicle, bicycle, pedestrian, train", type=str, required=True, default="vehicle")
+    parser.add_argument(
+        "--participant",
+        help="traffic participant type (one of vehicle, bicycle, pedestrian, train",
+        type=str,
+        required=True,
+        default="vehicle")
     parser.add_argument("--lat", help="Lateral position of origin", type=float, default=49)
     parser.add_argument("--lon", help="Longitudinal position of origin", type=float, default=8)
     args = parser.parse_args()
 
-    rules_map = {"vehicle" : lanelet2.traffic_rules.Participants.Vehicle,
-                 "bicycle" : lanelet2.traffic_rules.Participants.Bicycle,
-                 "pedestrian" : lanelet2.traffic_rules.Participants.Pedestrian,
-                 "train" : lanelet2.traffic_rules.Participants.Train}
+    rules_map = {"vehicle": lanelet2.traffic_rules.Participants.Vehicle,
+                 "bicycle": lanelet2.traffic_rules.Participants.Bicycle,
+                 "pedestrian": lanelet2.traffic_rules.Participants.Pedestrian,
+                 "train": lanelet2.traffic_rules.Participants.Train}
     proj = lanelet2.projection.UtmProjector(lanelet2.io.Origin(args.lat, args.lon))
     laneletmap = lanelet2.io.load(args.filename, proj)
 
