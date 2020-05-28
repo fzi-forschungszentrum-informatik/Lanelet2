28fb0a3412bbddde92a745c04691b50a5f605165
Fabian Poggenhans <fabian.poggenhans@kit.edu>
2020-05-28 23:30:42 +0200

    Add experimental support for building with colcon on ros2 and ament_cmake

--- lanelet2_examples/test/test_examples.py
+++ lanelet2_examples/test/test_examples.py
@@ -1,25 +1,28 @@
 import unittest
 import os
+import sys
 import subprocess
 
 
 class Lanelet2ExamplesTestCase(unittest.TestCase):
     def test_run_all_examples(self):
+        ros2 = os.environ.get("ROS_VERSION", "") == "2"
+        run_cmd = "rosrun lanelet2_examples {}" if not ros2 else "ros2 run lanelet2_examples {}"
         src = "../src"
         examples = [o for o in os.listdir(src) if os.path.isdir(os.path.join(src, o))]
         devnull = open(os.devnull, 'w')
         for example in examples:
             print("running " + example)
-            result = subprocess.call("rosrun lanelet2_examples {}".format(example), shell=True, stdout=devnull)
+            result = subprocess.call(run_cmd.format(example), shell=True)
             self.assertEqual(result, 0, "Failed to execute {}".format(example))
 
     def test_run_py_examples(self):
         scripts = "../scripts"
-        examples = [o for o in os.listdir(scripts) if not os.path.isdir(os.path.join(scripts, o))]
+        examples = [os.path.join(scripts, o) for o in os.listdir(scripts) if o.endswith(".py")]
         devnull = open(os.devnull, 'w')
         for example in examples:
             print("running " + example)
-            result = subprocess.call("rosrun lanelet2_examples {}".format(example), shell=True, stdout=devnull)
+            result = subprocess.call([sys.executable, example])
             self.assertEqual(result, 0, "Failed to execute {}".format(example))
 
 
