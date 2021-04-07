94af506978d7fc5b8477906a352312ae2cadc109
Maximilian Naumann <maximilian.naumann@kit.edu>
2021-04-07 12:46:44 +0200

    Name integration tests correctly and remove utils tests

--- lanelet2_matching/test/lanelet2_matching_integration.cpp
+++ lanelet2_matching/test/lanelet2_matching_integration.cpp
@@ -62,106 +62,11 @@ inline BasicPolygon2d absoluteHull(const BasicPolygon2d& relativeHull, const mat
 }
 }  // namespace
 
-class MatchingUtils : public ::testing::Test {
+class MatchingIntegrationTest : public ::testing::Test {
  public:
-  MatchingUtils() { map = load(exampleMapPath, projector); }
-  std::string exampleMapPath = std::string(PKG_DIR) + "/test/mapping_example.osm";
-  projection::UtmProjector projector{Origin({49, 8.4})};
-  LaneletMapPtr map;
-};
+  MatchingIntegrationTest() {
+    map = load(exampleMapPath, projector);
 
-TEST_F(MatchingUtils, fixtureSetupSuccessful) {  // NOLINT
-  EXPECT_TRUE(map->laneletLayer.exists(42440));
-}
-
-TEST_F(MatchingUtils, absoluteHull) {  // NOLINT
-  matching::Object2d obj;
-
-  obj.pose.translation() = BasicPoint2d{10, 0};                         //!< at point x=10 y=0
-  obj.pose.linear() = Eigen::Rotation2D<double>(-1.5 * M_PI).matrix();  //!< rotated by pi/2 (=-3pi/2)
-
-  obj.absoluteHull = absoluteHull(matching::Hull2d{BasicPoint2d{-0.5, -1}, BasicPoint2d{2, 1}}, obj.pose);
-
-  EXPECT_DOUBLE_EQ(10 + 1, obj.absoluteHull.at(0).x());
-  EXPECT_DOUBLE_EQ(0 - 0.5, obj.absoluteHull.at(0).y());
-
-  EXPECT_DOUBLE_EQ(10 - 1, obj.absoluteHull.at(1).x());
-  EXPECT_DOUBLE_EQ(0 + 2, obj.absoluteHull.at(1).y());
-}
-
-TEST_F(MatchingUtils, findWithin) {  // NOLINT
-  ASSERT_TRUE(map->pointLayer.exists(41656));
-  matching::Object2d obj;
-  obj.pose.translation() = map->pointLayer.get(41656).basicPoint2d();
-  obj.pose.linear() = Eigen::Rotation2D<double>(150. / 180. * M_PI).matrix();
-
-  EXPECT_EQ(4ul, matching::utils::findWithin(map->laneletLayer, obj, 0.1).size());
-
-  obj.absoluteHull = absoluteHull(
-      matching::Hull2d{BasicPoint2d{-1, -0.9}, BasicPoint2d{2, -0.9}, BasicPoint2d{2, 0.9}, BasicPoint2d{1, 0.9}},
-      obj.pose);
-
-  std::vector<std::pair<double, Lanelet>> laneletsWithDistance =
-      matching::utils::findWithin(map->laneletLayer, obj, 0.1);
-
-  EXPECT_EQ(4ul, laneletsWithDistance.size());
-
-  Ids laneletIds;
-  std::transform(laneletsWithDistance.begin(), laneletsWithDistance.end(), std::back_inserter(laneletIds),
-                 [](const auto& elem) { return elem.second.id(); });
-
-  EXPECT_TRUE(std::find(std::begin(laneletIds), std::end(laneletIds), 45356) != std::end(laneletIds))
-      << "Lanelet before the roundabout";
-  EXPECT_TRUE(std::find(std::begin(laneletIds), std::end(laneletIds), 45328) != std::end(laneletIds))
-      << "Lanelet leaving the roundabout";
-  EXPECT_TRUE(std::find(std::begin(laneletIds), std::end(laneletIds), 45334) != std::end(laneletIds))
-      << "Lanelet entering the roundabout";
-  EXPECT_TRUE(std::find(std::begin(laneletIds), std::end(laneletIds), 45344) != std::end(laneletIds))
-      << "Zebra crossing before the roundabout";
-  EXPECT_FALSE(std::find(std::begin(laneletIds), std::end(laneletIds), 45332) != std::end(laneletIds))
-      << "Lanelet inside the roundabout";
-}
-
-TEST_F(MatchingUtils, getMahalanobisDistSq) {  // NOLINT
-  ASSERT_TRUE(map->pointLayer.exists(41656));
-  matching::ObjectWithCovariance2d obj;
-  obj.pose.translation() = map->pointLayer.get(41656).basicPoint2d();
-  obj.pose.linear() = Eigen::Rotation2D<double>(150. / 180. * M_PI).matrix();
-  obj.absoluteHull = absoluteHull(
-      matching::Hull2d{BasicPoint2d{-1, -0.9}, BasicPoint2d{2, -0.9}, BasicPoint2d{2, 0.9}, BasicPoint2d{1, 0.9}},
-      obj.pose);
-
-  using namespace matching::utils;
-  obj.vonMisesKappa = 0.5;
-  EXPECT_THROW(getMahalanobisDistSq(map->laneletLayer.get(45356), obj), MatchingError)  // NOLINT
-      << "should throw on covariance = zero";
-
-  obj.positionCovariance = matching::PositionCovariance2d::Ones();
-  EXPECT_THROW(getMahalanobisDistSq(map->laneletLayer.get(45356), obj), MatchingError)  // NOLINT
-      << "should throw on determinant = zero";
-
-  obj.positionCovariance = matching::PositionCovariance2d::Identity() * 2.;
-  obj.vonMisesKappa = 1. / (10. / 180. * M_PI);  // covariance of 10 degrees
-
-  double mahaDist45334 = getMahalanobisDistSq(map->laneletLayer.get(45334), obj);
-  EXPECT_NEAR(0.28817716004330124, mahaDist45334, 10e-6) << "Lanelet entering the roundabout";
-
-  double mahaDist45356 = getMahalanobisDistSq(map->laneletLayer.get(45356).invert(), obj);
-  EXPECT_NEAR(3.2487924075660697, mahaDist45356, 10e-6) << "Lanelet before the roundabout";
-
-  double mahaDist45328 = getMahalanobisDistSq(map->laneletLayer.get(45328).invert(), obj);
-  EXPECT_NEAR(11.919014409926168, mahaDist45328, 10e-6) << "Lanelet leaving the roundabout";
-
-  double mahaDist45344 = getMahalanobisDistSq(map->laneletLayer.get(45344), obj);
-  EXPECT_NEAR(57.419088438833477, mahaDist45344, 10e-6) << "Zebra crossing before the roundabout";
-
-  double mahaDist45332 = getMahalanobisDistSq(map->laneletLayer.get(45332), obj);
-  EXPECT_NEAR(47.481075513744784, mahaDist45332, 10e-6) << "Lanelet inside the roundabout";
-}
-
-class Matching : public MatchingUtils {
- public:
-  Matching() {
     obj.pose.translation() = map->pointLayer.get(41656).basicPoint2d();
     obj.pose.linear() = Eigen::Rotation2D<double>(150. / 180. * M_PI).matrix();
     obj.absoluteHull = absoluteHull(
@@ -171,9 +76,16 @@ class Matching : public MatchingUtils {
     obj.vonMisesKappa = 1. / (10. / 180. * M_PI);  // covariance of 10 degrees
   }
   matching::ObjectWithCovariance2d obj;
+  std::string exampleMapPath = std::string(PKG_DIR) + "/test/mapping_example.osm";
+  projection::UtmProjector projector{Origin({49, 8.4})};
+  LaneletMapPtr map;
 };
 
-TEST_F(Matching, deterministicNonConst) {  // NOLINT
+TEST_F(MatchingIntegrationTest, fixtureSetupSuccessful) {  // NOLINT
+  EXPECT_TRUE(map->laneletLayer.exists(42440));
+}
+
+TEST_F(MatchingIntegrationTest, deterministicNonConst) {  // NOLINT
   using namespace lanelet::matching;
   auto matches = getDeterministicMatches(*map, obj, 4.);
   for (size_t i = 1; i < matches.size(); i++) {
@@ -188,14 +100,14 @@ TEST_F(Matching, deterministicNonConst) {  // NOLINT
   EXPECT_EQ(45330, matches.at(9).lanelet.id());
 }
 
-TEST_F(Matching, deterministicConst) {  // NOLINT
+TEST_F(MatchingIntegrationTest, deterministicConst) {  // NOLINT
   using namespace lanelet::matching;
   const LaneletMap& constMap = *map;
   auto matches = getDeterministicMatches(constMap, obj, 4.);
   EXPECT_EQ(14ul, matches.size());
 }
 
-TEST_F(Matching, probabilisticNonConst) {  // NOLINT
+TEST_F(MatchingIntegrationTest, probabilisticNonConst) {  // NOLINT
   using namespace lanelet::matching;
   auto matches = getProbabilisticMatches(*map, obj, 4.);
   // NOLINTNEXTLINE(clang-analyzer-cplusplus.NewDelete)
@@ -208,14 +120,14 @@ TEST_F(Matching, probabilisticNonConst) {  // NOLINT
   EXPECT_EQ(14ul, matches.size());
 }
 
-TEST_F(Matching, probabilisticConst) {  // NOLINT
+TEST_F(MatchingIntegrationTest, probabilisticConst) {  // NOLINT
   using namespace lanelet::matching;
   const LaneletMap& constMap = *map;
   auto matches = getProbabilisticMatches(constMap, obj, 4.);
   EXPECT_EQ(14ul, matches.size());
 }
 
-TEST_F(Matching, isCloseTo) {  // NOLINT
+TEST_F(MatchingIntegrationTest, isCloseTo) {  // NOLINT
   using namespace lanelet::matching;
 
   LaneletLayer emptyLayer;
@@ -230,7 +142,7 @@ TEST_F(Matching, isCloseTo) {  // NOLINT
   EXPECT_TRUE(matching::isCloseTo(map->laneletLayer, objWithEmptyHull, 1.));
 }
 
-TEST_F(Matching, filterNonCompliantProbabilistic) {  // NOLINT
+TEST_F(MatchingIntegrationTest, filterNonCompliantProbabilistic) {  // NOLINT
   using namespace lanelet::matching;
   auto matches = getProbabilisticMatches(*map, obj, 4.);
   EXPECT_EQ(14ul, matches.size());
@@ -245,7 +157,7 @@ TEST_F(Matching, filterNonCompliantProbabilistic) {  // NOLINT
   EXPECT_EQ("45334 45356inv 45358inv 45332 45330 45328 45356 45358 ", toString(compliantMatches));
 }
 
-TEST_F(Matching, filterNonCompliantDeterminstic) {  // NOLINT
+TEST_F(MatchingIntegrationTest, filterNonCompliantDeterminstic) {  // NOLINT
   using namespace lanelet::matching;
   auto matches = getDeterministicMatches(*map, obj, 4.);
   EXPECT_EQ(14ul, matches.size());
