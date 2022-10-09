0778d6a535e2a3fbf04ee11ed03484b60a90d50f
Fabian Immel <fabian.immel@kit.edu>
2022-10-09 16:30:53 +0200

    bugfixes lcov bash script

--- .github/lcov_merge.bash
+++ .github/lcov_merge.bash
@@ -1,21 +1,26 @@
 #!/bin/bash
+set -ex
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 LANELET2_ROOT=$(dirname $SCRIPT_DIR)
+WORKSPACE_ROOT=$(dirname $(dirname $LANELET2_ROOT))
 
 src_dirs=""
-for d in ${LANELET2_ROOT}/build/*/ ; do
-    [ "$(basename $d)" = "catkin_tools_prebuild" ] && continue
-    pkg="$(basename $d)"
-    src_dir=$(find ${PWD}/src -type d -name ${pkg} -prune)
-    src_dirs="${src_dirs} ${src_dir}/*"
+for d in ${LANELET2_ROOT}/*/ ; do
+    [ "$(basename $d)" = ".github" ] && continue
+    [ "$(basename $d)" = "lanelet2" ] && continue
+    src_dirs="${src_dirs} ${d}*"
 done
 
+echo "$src_dirs"
+
 lcov_filtered_files=""
-for d in ${LANELET2_ROOT}/build/*/ ; do
-    [ "$(basename $d)" = "catkin_tools_prebuild" ] && continue
-    file="${d}mrt_coverage/full_coverage.lcov"
+for d in ${LANELET2_ROOT}/*/ ; do
+    [ "$(basename $d)" = ".github" ] && continue
+    [ "$(basename $d)" = "lanelet2" ] && continue
+    pkg="$(basename $d)"
+    build_dir=$(find ${WORKSPACE_ROOT}/build -type d -name ${pkg} -prune)
+    file="${build_dir}/mrt_coverage/full_coverage.lcov"
     if [ -f "$file" ]; then
-        pkg="$(basename $d)"
         filtered_path="${d}mrt_coverage/full_coverage_filtered.lcov"
 
         set -f
@@ -26,5 +31,5 @@ for d in ${LANELET2_ROOT}/build/*/ ; do
     fi;
 done
 
-mkdir /tmp/lcov
+mkdir -p /tmp/lcov
 lcov -o /tmp/lcov/full_coverage.lcov ${lcov_filtered_files}
