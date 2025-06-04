c224f39f640ea282703a2ed6da5a1b5c2ee973eb
Richard Fehler <richard.fehler@kit.edu>
2025-06-04 10:51:52 +0200

    restore unwanted changes

--- lanelet2_python/scripts/create_debug_routing_graph.py
+++ lanelet2_python/scripts/create_debug_routing_graph.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 import lanelet2
 import sys
@@ -23,6 +23,8 @@ if __name__ == "__main__":
                  "pedestrian": lanelet2.traffic_rules.Participants.Pedestrian,
                  "train": lanelet2.traffic_rules.Participants.Train}
     proj = lanelet2.projection.UtmProjector(lanelet2.io.Origin(args.lat, args.lon))
+    laneletmap = lanelet2.io.load(args.filename, proj)
+
     routing_cost = lanelet2.routing.RoutingCostDistance(0.)  # zero cost for lane changes
     traffic_rules = lanelet2.traffic_rules.create(lanelet2.traffic_rules.Locations.Germany,
                                                   rules_map[args.participant])
